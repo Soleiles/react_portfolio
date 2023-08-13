a4c9bb582dd240e91f14006dd31fecaee3c45f18
Soleiles <ktaing789@gmail.com>
2023-08-12 23:53:13 -0400

    added navbar and routes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,47 @@
-export default function Nav(){
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function Navigation() {
+  const currentPage = useLocation().pathname;
+
   return (
-    <nav>
-      Nav
-    </nav>
-  )
-};
+    <header>
+      <Navbar expand='lg' className='nav-bar'>
+        <Container>
+          <Navbar.Brand className='full-name'>Kevin Taing</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto tabs'>
+              <Link
+                to="/"
+                className={currentPage === "/" ? "nav-link active" : "nav-link"}
+              >
+                About Me
+              </Link>
+              <Link
+                to="/portfolio"
+                className={currentPage === "/portfolio" ? "nav-link active" : "nav-link"}
+              >
+                Portfolio
+              </Link>
+              <Link
+                to="/contact"
+                className={currentPage === "/contact" ? "nav-link active" : "nav-link"}
+              >
+                Contact
+              </Link>
+              <Link
+                to="/resume"
+                className={currentPage === "/resume" ? "nav-link active" : "nav-link"}
+              >
+                Resume
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import App from './App.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+import Contact from './pages/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: "/aboutme",
+        element: <AboutMe />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
