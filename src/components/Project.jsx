852ff993bd844e8d02a783de34a7f9e58bdc3d38
Soleiles <ktaing789@gmail.com>
2023-09-05 20:17:04 -0400

    added project 3 to portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
+import HabitYourWay from "../assets/images/habit-your-way.png";
 import Bookbusta from "../assets/images/bookbusta.png";
 import Recipedia from "../assets/images/recipedia.png";
 import NoteTaker from "../assets/images/expressjs-note-taker.png";
@@ -11,45 +12,45 @@ import Scheduler from "../assets/images/work-day-scheduler.png";
 const projects = [
     {
         id: "1",
+        title: "Habit Tracker",
+        image: HabitYourWay,
+        app: "https://habit-your-way-habit-tracker-8b86617a280e.herokuapp.com/",
+        repo: "https://github.com/slmov215/habit-your-way",
+    },
+    {
+        id: "2",
         title: "Bookbusta",
         image: Bookbusta,
         app: "https://bookbusta-950538aa625f.herokuapp.com/",
         repo: "https://github.com/Nneifem/bookbusta",
     },
     {
-        id: "2",
+        id: "3",
         title: "Recipedia",
         image: Recipedia,
         app: "https://ruggerheist.github.io/recipedia/",
         repo: "https://github.com/ruggerheist/recipedia",
     },
     {
-        id: "3",
+        id: "4",
         title: "Express.js Note Taker",
         image: NoteTaker,
         app: "https://express-js-note-taker-project.herokuapp.com/",
         repo: "https://github.com/Soleiles/express.js-note-taker",
     },
     {
-        id: "4",
+        id: "5",
         title: "Javascrupt Coding Quiz",
         image: CodeQuiz,
         app: "https://soleiles.github.io/code-quiz/",
         repo: "https://github.com/Soleiles/code-quiz",
     },
     {
-        id: "5",
+        id: "6",
         title: "Password Generator",
         image: PasswordGen,
         app: "https://soleiles.github.io/Password-Generator/",
         repo: "https://github.com/Soleiles/Password-Generator",
-    },
-    {
-        id: "6",
-        title: "Work Day Scheduler",
-        image: Scheduler,
-        app: "https://soleiles.github.io/Work-Day_Scheduler/",
-        repo: "https://github.com/Soleiles/Work-Day_Scheduler",
     },
 ];
 
